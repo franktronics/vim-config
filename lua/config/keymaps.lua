acdd0cf26c92f296fbcd323fa64306cab39c986d
franktronicsprog <franktronicsprog@gmail.com>
2025-09-26 09:43:51 +0200

    feat: add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any a dditional keymaps here 0
+--
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/Decrement
+keymap.set("n", "+", "<C-a>", { desc = "Increment" })
+keymap.set("n", "+", "<C-a>", { desc = "Decrement" })
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all the page" })
+
+-- Escape insert mode
+keymap.set("i", "jj", "<Esc>", { desc = "Return to normal mode" })
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-S-h>", "<C-w><")
+keymap.set("n", "<C-S-l>", "<C-w>>")
+keymap.set("n", "<C-S-k>", "<C-w>+")
+keymap.set("n", "<C-S-j>", "<C-w>-")
